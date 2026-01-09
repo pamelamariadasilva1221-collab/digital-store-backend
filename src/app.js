ab66aba64541f688fc9faaa341c35dd124abb7e4
pamelamariadasilva1221-collab <pamelamariadasilva1221@gmail.com>
2026-01-08 21:32:45 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
+// Importa as rotas
+const userRoutes = require("./routes/userRoutes");
+
 app.use(express.json());
 
+// Usa as rotas
+app.use("/api", userRoutes);
+
+// Rota de teste
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "API funcionando" });
+  res.status(200).json({
+    message: "API funcionando corretamente 🚀"
+  });
 });
 
 app.listen(3000, () => {
